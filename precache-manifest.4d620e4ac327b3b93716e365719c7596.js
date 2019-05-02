de8117587e05ca37d8ea238d28c8ef59e0cb3154
chettoy <chettoy@with.moe>
2019-05-02 04:12:11 +0000

    Updates

---
+++ precache-manifest.4d620e4ac327b3b93716e365719c7596.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d1c3fba646e30c76cc889ee818046f",
+    "url": "/my-note/index.html"
+  },
+  {
+    "revision": "e93dd2fcc45f8a9ba295",
+    "url": "/my-note/static/css/main.02a3573b.chunk.css"
+  },
+  {
+    "revision": "2e4e2cdd4bf4074ef22c",
+    "url": "/my-note/static/js/2.9d7c9682.chunk.js"
+  },
+  {
+    "revision": "e93dd2fcc45f8a9ba295",
+    "url": "/my-note/static/js/main.73e58ff4.chunk.js"
+  },
+  {
+    "revision": "bbe9740ef1c51f8b3b3c",
+    "url": "/my-note/static/js/runtime~main.5c54fb4d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-note/precache-manifest.37c5a855de84ebf0ea81c1a4fa1f291d.js"
+  "/my-note/precache-manifest.4d620e4ac327b3b93716e365719c7596.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/my-note/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-note/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
