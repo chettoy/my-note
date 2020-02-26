f0b9b6c5b92b4cdbb4f2e942e515ad6510f679a6
chettoy <chettoy@with.moe>
2020-02-26 08:27:19 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-note/precache-manifest.f456801e827839f6ee43954327861b2a.js"
+  "/my-note/precache-manifest.2e076d78da0d8950116c52a94629a441.js"
 );
 
 self.addEventListener('message', (event) => {
