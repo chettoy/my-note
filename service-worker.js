042ad50d3889a7c10a77f51f475a54e15d9b9633
chettoy <chettoy@with.moe>
2020-08-22 01:49:26 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,7 @@ if (workbox) {
   workbox.core.clientsClaim();
 
   /* injection point for manifest files.  */
-  workbox.precaching.precacheAndRoute([{"revision":"c1c28f553095fdddb4d2c13a11bd4cb9","url":"live2d-widget/live2d.min.js"},{"revision":"1ab8aaf74cb0cafc3257c6ffbd027570","url":"live2d-widget/waifu-tips.json"},{"revision":"1165b1888975a8d6ee01539b6ddefeb6","url":"static/css/2.8c4379df.chunk.css"},{"revision":"cc51ebd73f341dcaf8f7fa5ebd38def7","url":"static/css/3.bbfc64cf.chunk.css"},{"revision":"15db525a3e5ffd6b378b57022c5f7cfd","url":"static/css/main.b4dd0116.chunk.css"},{"revision":"f7e779e78755f3e3c4f92d9c5781a9bf","url":"static/js/2.bb4a3fbf.chunk.js"},{"revision":"f505e90cbcc2e5e1910e45db7e28c455","url":"static/js/3.6c990c75.chunk.js"},{"revision":"1a06a7c4eb93a26c8bc9771ab3092534","url":"static/js/4.2d8e2951.chunk.js"},{"revision":"1db803863fa336be79e1ccd1850a48af","url":"static/js/main.269b3aca.chunk.js"},{"revision":"14886cc3b07f63d6a2c9c4ea87634266","url":"static/js/runtime-main.746e8b35.js"},{"revision":"d04c129bcf91745767f64c1393b451fa","url":"index.html"}]);
+  workbox.precaching.precacheAndRoute([{"revision":"c1c28f553095fdddb4d2c13a11bd4cb9","url":"live2d-widget/live2d.min.js"},{"revision":"1ab8aaf74cb0cafc3257c6ffbd027570","url":"live2d-widget/waifu-tips.json"},{"revision":"1165b1888975a8d6ee01539b6ddefeb6","url":"static/css/2.8c4379df.chunk.css"},{"revision":"cc51ebd73f341dcaf8f7fa5ebd38def7","url":"static/css/3.bbfc64cf.chunk.css"},{"revision":"15db525a3e5ffd6b378b57022c5f7cfd","url":"static/css/main.b4dd0116.chunk.css"},{"revision":"f7e779e78755f3e3c4f92d9c5781a9bf","url":"static/js/2.bb4a3fbf.chunk.js"},{"revision":"fc3e5c1e902d5abd100f20d0878018c9","url":"static/js/3.d47ab01a.chunk.js"},{"revision":"1a06a7c4eb93a26c8bc9771ab3092534","url":"static/js/4.2d8e2951.chunk.js"},{"revision":"b5da62e8786747db0e90d7aa2266fc9e","url":"static/js/main.c5378932.chunk.js"},{"revision":"0384e11aedee9b00f0395de7c03a0ec5","url":"static/js/runtime-main.24f56593.js"},{"revision":"5156dc7eeaa5333ef10b363e58947ee9","url":"index.html"}]);
 
   /* custom cache rules */
   workbox.routing.registerRoute(
@@ -26,28 +26,28 @@ if (workbox) {
   workbox.routing.registerRoute(
     ({url}) => url.origin === 'https://cdn.jsdelivr.net',
     new workbox.strategies.StaleWhileRevalidate({
-      cacheName: 'cdn-jsdelivr',
+      cacheName: 'jsdelivr-cdn',
     }),
   );
 
   // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-  registerRoute(
+  workbox.routing.registerRoute(
     ({url}) => url.origin === 'https://fonts.googleapis.com',
-    new StaleWhileRevalidate({
+    new workbox.strategies.StaleWhileRevalidate({
       cacheName: 'google-fonts-stylesheets',
     })
   );
 
   // Cache the underlying font files with a cache-first strategy for 1 year.
-  registerRoute(
+  workbox.routing.registerRoute(
     ({url}) => url.origin === 'https://fonts.gstatic.com',
-    new CacheFirst({
+    new workbox.strategies.CacheFirst({
       cacheName: 'google-fonts-webfonts',
       plugins: [
-        new CacheableResponsePlugin({
+        new workbox.cacheableResponse.CacheableResponsePlugin({
           statuses: [0, 200],
         }),
-        new ExpirationPlugin({
+        new workbox.expiration.ExpirationPlugin({
           maxAgeSeconds: 60 * 60 * 24 * 365,
           maxEntries: 30,
         }),
