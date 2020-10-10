c27c5fb61ad93ba4ec04b5d9e3d1acd228cf6168
chettoy <chettoy@with.moe>
2020-10-10 06:41:22 +0000

    Updates

---
+++ precache-manifest.ffc86da4de5ba87eae8ef45dd660aefc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc82002996fc9f772cbd0758ba7aad59",
+    "url": "/my-note/index.html"
+  },
+  {
+    "revision": "ddde86b9e30fccd511e5",
+    "url": "/my-note/static/css/2.8c4379df.chunk.css"
+  },
+  {
+    "revision": "a09295e253d5ef8a8209",
+    "url": "/my-note/static/css/3.bbfc64cf.chunk.css"
+  },
+  {
+    "revision": "6076e404667913503407",
+    "url": "/my-note/static/css/main.f42fae53.chunk.css"
+  },
+  {
+    "revision": "ddde86b9e30fccd511e5",
+    "url": "/my-note/static/js/2.429f1711.chunk.js"
+  },
+  {
+    "revision": "2688949da1d8cdef32d3ce405bac2a77",
+    "url": "/my-note/static/js/2.429f1711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09295e253d5ef8a8209",
+    "url": "/my-note/static/js/3.2036d261.chunk.js"
+  },
+  {
+    "revision": "b237561c4b6de73918f3",
+    "url": "/my-note/static/js/4.fab92389.chunk.js"
+  },
+  {
+    "revision": "6076e404667913503407",
+    "url": "/my-note/static/js/main.fc5dd53f.chunk.js"
+  },
+  {
+    "revision": "a9d23a04e50ff9f2a632",
+    "url": "/my-note/static/js/runtime-main.e31018df.js"
+  }
+]);
