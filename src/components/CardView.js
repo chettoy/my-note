f26a0af67e7518d0059f78560f812f523707f453
chettoy <chettoy@with.moe>
2021-07-12 18:10:57 +0800

    Update

--- src/components/CardView.js
+++ src/components/CardView.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components/macro';
+import styles from './CardView.module.scss';
 
-const CardView = styled.div`
+const CardView = styled.div.attrs({
+  className: styles.CardView
+})`
   background: ${props => props.theme.CardColor};
   color: ${props => props.theme.TextColor};
-  box-shadow: 0 0.2rem 0.2rem rgba(0,0,0,0.54);
-  padding: 0.625rem 1rem;
-  margin: 1rem auto;
-  box-sizing: border-box;
 `;
 
 CardView.defaultProps = {
